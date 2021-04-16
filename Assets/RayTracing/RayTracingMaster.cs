78742b8b16161882fa258fb3d2ad07d8afdf6775
NamNH <hainam125@gmail.com>
2021-04-16 20:09:59 +0700

    Init

---
+++ Assets/RayTracing/RayTracingMaster.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour {
+    [SerializeField] private ComputeShader rayTracingShader;
+    [SerializeField] private Texture2D skyboxTexture;
+
+    private RenderTexture target;
+    private new Camera camera;
+
+    private void Awake() {
+        camera = GetComponent<Camera>();
+    }
+
+    private void SetShaderParameters() {
+        rayTracingShader.SetTexture(0, "_SkyboxTexture", skyboxTexture);
+        rayTracingShader.SetMatrix("_CameraToWorld", camera.cameraToWorldMatrix);
+        rayTracingShader.SetMatrix("_CameraInverseProjection", camera.projectionMatrix.inverse);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    private void Render(RenderTexture destination) {
+        InitRenderTexture();
+
+        rayTracingShader.SetTexture(0, "Result", target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8f);
+        rayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        Graphics.Blit(target, destination);
+    }
+
+    private void InitRenderTexture() {
+        if(target == null || target.width != Screen.width || target.height != Screen.height) {
+            if (target != null) target.Release();
+
+            target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+    }
+}
